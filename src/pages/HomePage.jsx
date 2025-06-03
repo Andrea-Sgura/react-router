18786cbf1dae2de40e98d0dcf67e27d4ea75ab6e
Andrea-Sgura <andreasgura96@gmail.com>
2025-06-03 08:22:01 +0200

    [UPD] Creata componente Homepage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Homepage</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sed molestiae, ducimus quia nobis a illum error corporis voluptatum praesentium illo possimus iure odit ullam doloribus magnam vel architecto beatae.</p>
+        </div>
+    )
+}
+
+export default HomePage
