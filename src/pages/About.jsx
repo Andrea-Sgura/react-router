8ce3ac2e263547d7cd147cc2a52e554e63cbd3c0
Andrea-Sgura <andreasgura96@gmail.com>
2025-06-03 09:00:50 +0200

    [UPD] Importata Navbar in ABout.jsx + Product.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import MainNavbar from '../components/MainNavbar'
 
 const About = () => {
     return (
         <div>
+            <MainNavbar />
             <h1>About Us</h1>
         </div>
     )
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import MainNavbar from '../components/MainNavbar'
 
 const Products = () => {
   return (
     <div>
+      <MainNavbar />
       <h1>Elenco prodotti</h1>
     </div>
   )
