69bb291fbf66d8b5c9af3f3a6a8fde462e623900
Andrea-Sgura <andreasgura96@gmail.com>
2025-06-03 08:56:27 +0200

    [UPD] Impostata struttura navbar, importate rotte in app.jsx tramite BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import HomePage from './pages/HomePage'
+import About from './pages/About'
+import Products from './pages/Products'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={HomePage}></Route>
+          <Route path="/about" Component={About}></Route>
+          <Route path="/products" Component={Products}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import MainNavbar from '../components/MainNavbar'
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Homepage</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sed molestiae, ducimus quia nobis a illum error corporis voluptatum praesentium illo possimus iure odit ullam doloribus magnam vel architecto beatae.</p>
-        </div>
+        <>
+            <MainNavbar />
+            <div>
+                <h1>Homepage</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sed molestiae, ducimus quia nobis a illum error corporis voluptatum praesentium illo possimus iure odit ullam doloribus magnam vel architecto beatae.</p>
+            </div>
+        </>
     )
 }
 
